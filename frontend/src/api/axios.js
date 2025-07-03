3922ac34403ce05524931804e4f52014b3a11a64
Varun Rawat <varunrawat5758@gmail.com>
2025-07-03 11:55:01 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://snippet-manager-wpb2.onrender.com/api/v1",
   withCredentials: true,
 });
 
